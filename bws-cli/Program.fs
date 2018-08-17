6c72f628c20b895c425cb36e44e891559f1e1313
Tobias Becker <T.Becker@bioid.com>
2018-08-17 11:07:33 +0200

    Help parameter was not respected.

--- bws-cli/Program.fs
+++ bws-cli/Program.fs
@@ -28,144 +28,148 @@ let main argv =
         |> Array.toList
         |> CommandLine.parse
 
-    if opts.AppId = Options.Default.AppId || opts.Secret = Options.Default.Secret then
-        eprintfn "\nERROR: Both application id and secret need to be passed to connect to BWS!\n"
-        Help.specific.["tool"] |> eprintfn "%s"
-        int RetCode.Unauthorized
+    if opts.Help then
+        Help.specific.["tool"] |> printfn "\n%s\n"
+        0
     else
-        let args, opts =
-            if List.isEmpty args then
-                // I there are no commands via the command line, we switch to interactive.
-                "", {opts with Interactive = true }
-            else
-                // Assemble words back into a line while transforming to lower.
-                args
-                |> List.map (fun str -> str.ToLowerInvariant())
-                |> String.concat " ", opts
-
-        let printf fmt = Print.printf (not opts.Quiet) fmt
-        let printfn fmt = Print.printfn (not opts.Quiet) fmt
-
-        let input =
-            let lead = printf ">>> %s"
-            let rec consoleLines before = seq {
-                if before <> "" then
-                    // before is the line that was passed via the command line.
-                    lead before
-                    printfn ""
-                    yield before
-                lead ""
-                match Console.ReadLine() with
-                | null -> ()
-                | line ->
-                    match line.Trim().ToLowerInvariant() with
-                    | Command(Long "exit")
-                    | "quit" ->
-                        printfn "Bye!"
-                    | line ->
-                        yield line
-                        yield! consoleLines ""
-            }
-            (
-                if opts.Interactive then
-                    printfn "--- Interactive BWS console"
-                    consoleLines args
+        if opts.AppId = Options.Default.AppId || opts.Secret = Options.Default.Secret then
+            eprintfn "\nERROR: Both application id and secret need to be passed to connect to BWS!\n"
+            Help.specific.["tool"] |> eprintfn "%s"
+            int RetCode.Unauthorized
+        else
+            let args, opts =
+                if List.isEmpty args then
+                    // I there are no commands via the command line, we switch to interactive.
+                    "", {opts with Interactive = true }
                 else
-                    [args] :> _
-            )
-            // Split a multi-line string into lines.
-            |> Seq.map split
-            // We neither want to process empty lines...
-            |> Seq.filter (List.isEmpty >> not)
-            // not comment lines.
-            |> Seq.filter notComments
-            // Rewrite these four commands with file arguments to upload command followed by the original one.
-            |> Seq.collect (function
-                | Command(Long("verify" as cmd)) :: files
-                | Command(Long("enroll" as cmd)) :: files
-                | Command(Long("identify" as cmd)) :: files
-                | Command(Long("livenessdetection" as cmd)) :: files ->
-                    [
-                        let args, files =
-                            files
-                            |> List.partition (glob >> Seq.isEmpty)
-                        yield "upload" :: files
-                        yield cmd :: args
-                    ]
-                | unchanged -> [unchanged]
-            )
+                    // Assemble words back into a line while transforming to lower.
+                    args
+                    |> List.map (fun str -> str.ToLowerInvariant())
+                    |> String.concat " ", opts
 
-        /// Remember all created BCIDs, so that we can remove them later.
-        let bcids = ResizeArray<string>()
+            let printf fmt = Print.printf (not opts.Quiet) fmt
+            let printfn fmt = Print.printfn (not opts.Quiet) fmt
 
-        try
-            let bwsResults =
-                input
-                // This is the meat of the program.
-                |> Seq.map (Api.call opts bcids)
-                // We cannot mix or overlap commands, because they probably depend on each other.
-                |> Seq.map Async.RunSynchronously
-            
-            let combine (source:seq<_>) = seq {
-                use enm = source.GetEnumerator()
-                let mutable prev = None
-                let mutable stop = false
-                while enm.MoveNext() && not stop do
-                    match prev, enm.Current with
-                    | Some (Code code), Expect exp ->
-                        if code = exp then
-                            printfn "Expectation matched."
-                            yield RetCode.Ok
-                        else
-                            printf "Expected %O but got %O" exp code
-                            if opts.Interactive then
-                                printfn "."
-                            else
-                                printfn ", exiting..."
-                            stop <- true
-                            yield code
-                    | Some (Code code), Code _ ->
-                        yield code
-                    | _ -> ()
-                    prev <- Some enm.Current
-                match stop, prev with
-                | false, Some (Code code) -> yield code
-                | _ -> ()
-            }
-
-            let combined = bwsResults |> combine
-
-            // We can only return a single error code. I decided for the first one.
-            let firstBad =
-                combined
-                // Why list? Because I want to evaluate everything, but Seq is lazy.
-                |> Seq.toList
-                |> List.tryFind ((<>)RetCode.Ok)
-            
-            match firstBad with
-            | Some code ->
-                Debuggy.WriteLine("Exiting with %O (%i).", code, int code)
-                int code
-            | None ->
-                Debuggy.WriteLine("Exiting normally.")
-                0
-        finally
-            // Extract all BCIDs that were enrolled (without the dot, the token didn't go through).
-            let rec createdBcids i = seq {
-                if i >= 1 then
-                    if bcids.[i] = "." && bcids.[i - 1] <> "." then
-                        yield bcids.[i - 1]
-                        yield! createdBcids (i - 2)
+            let input =
+                let lead = printf ">>> %s"
+                let rec consoleLines before = seq {
+                    if before <> "" then
+                        // before is the line that was passed via the command line.
+                        lead before
+                        printfn ""
+                        yield before
+                    lead ""
+                    match Console.ReadLine() with
+                    | null -> ()
+                    | line ->
+                        match line.Trim().ToLowerInvariant() with
+                        | Command(Long "exit")
+                        | "quit" ->
+                            printfn "Bye!"
+                        | line ->
+                            yield line
+                            yield! consoleLines ""
+                }
+                (
+                    if opts.Interactive then
+                        printfn "--- Interactive BWS console"
+                        consoleLines args
                     else
-                        yield! createdBcids (i - 1)
+                        [args] :> _
+                )
+                // Split a multi-line string into lines.
+                |> Seq.map split
+                // We neither want to process empty lines...
+                |> Seq.filter (List.isEmpty >> not)
+                // not comment lines.
+                |> Seq.filter notComments
+                // Rewrite these four commands with file arguments to upload command followed by the original one.
+                |> Seq.collect (function
+                    | Command(Long("verify" as cmd)) :: files
+                    | Command(Long("enroll" as cmd)) :: files
+                    | Command(Long("identify" as cmd)) :: files
+                    | Command(Long("livenessdetection" as cmd)) :: files ->
+                        [
+                            let args, files =
+                                files
+                                |> List.partition (glob >> Seq.isEmpty)
+                            yield "upload" :: files
+                            yield cmd :: args
+                        ]
+                    | unchanged -> [unchanged]
+                )
+
+            /// Remember all created BCIDs, so that we can remove them later.
+            let bcids = ResizeArray<string>()
+
+            try
+                let bwsResults =
+                    input
+                    // This is the meat of the program.
+                    |> Seq.map (Api.call opts bcids)
+                    // We cannot mix or overlap commands, because they probably depend on each other.
+                    |> Seq.map Async.RunSynchronously
+            
+                let combine (source:seq<_>) = seq {
+                    use enm = source.GetEnumerator()
+                    let mutable prev = None
+                    let mutable stop = false
+                    while enm.MoveNext() && not stop do
+                        match prev, enm.Current with
+                        | Some (Code code), Expect exp ->
+                            if code = exp then
+                                printfn "Expectation matched."
+                                yield RetCode.Ok
+                            else
+                                printf "Expected %O but got %O" exp code
+                                if opts.Interactive then
+                                    printfn "."
+                                else
+                                    printfn ", exiting..."
+                                stop <- true
+                                yield code
+                        | Some (Code code), Code _ ->
+                            yield code
+                        | _ -> ()
+                        prev <- Some enm.Current
+                    match stop, prev with
+                    | false, Some (Code code) -> yield code
+                    | _ -> ()
                 }
 
-            if opts.CleanUp then
-                bcids.Count - 1
-                |> createdBcids
-                |> Seq.toList
-                |> Seq.distinct
-                |> Seq.map (fun id -> Api.call { opts with Verbosity = 0; PrintResponseBody = false } bcids ["deleteclass"; id])
-                |> Async.Parallel
-                |> Async.Ignore
-                |> Async.RunSynchronously
+                let combined = bwsResults |> combine
+
+                // We can only return a single error code. I decided for the first one.
+                let firstBad =
+                    combined
+                    // Why list? Because I want to evaluate everything, but Seq is lazy.
+                    |> Seq.toList
+                    |> List.tryFind ((<>)RetCode.Ok)
+            
+                match firstBad with
+                | Some code ->
+                    Debuggy.WriteLine("Exiting with %O (%i).", code, int code)
+                    int code
+                | None ->
+                    Debuggy.WriteLine("Exiting normally.")
+                    0
+            finally
+                // Extract all BCIDs that were enrolled (without the dot, the token didn't go through).
+                let rec createdBcids i = seq {
+                    if i >= 1 then
+                        if bcids.[i] = "." && bcids.[i - 1] <> "." then
+                            yield bcids.[i - 1]
+                            yield! createdBcids (i - 2)
+                        else
+                            yield! createdBcids (i - 1)
+                    }
+
+                if opts.CleanUp then
+                    bcids.Count - 1
+                    |> createdBcids
+                    |> Seq.toList
+                    |> Seq.distinct
+                    |> Seq.map (fun id -> Api.call { opts with Verbosity = 0; PrintResponseBody = false } bcids ["deleteclass"; id])
+                    |> Async.Parallel
+                    |> Async.Ignore
+                    |> Async.RunSynchronously
